1f46564edbaa2b6bd6612ce42db0a8c4b629d7b2
KJ OBrien <kjokjo219@gmail.com>
2026-08-11 19:38:25 -0400

    adds the routing for my bubbles

--- cfpbubble/src/app/navigation/navigation.component.ts
+++ cfpbubble/src/app/navigation/navigation.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css'
 })
