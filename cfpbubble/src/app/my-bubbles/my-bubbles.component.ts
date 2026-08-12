ed8375effd58c713c2475b876d800faa950f064b
KJ OBrien <kjokjo219@gmail.com>
2026-08-11 20:01:03 -0400

    adds styling and seraching for my bubbles

--- cfpbubble/src/app/my-bubbles/my-bubbles.component.ts
+++ cfpbubble/src/app/my-bubbles/my-bubbles.component.ts
@@ -1,11 +1,47 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { BubbleService } from '../services/bubble.service';
+import { TeamService } from '../services/team.service';
 
 @Component({
   selector: 'app-my-bubbles',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    RouterModule
+  ],
   templateUrl: './my-bubbles.component.html',
   styleUrl: './my-bubbles.component.css'
 })
 export class MyBubblesComponent {
+
+  searchEmail = '';
+  bubbles: any[] = []
+
+  constructor(private teamService: TeamService, private bubbleService: BubbleService) { }
+
+  searchBubbles(): void {
+
+    const email = this.searchEmail.trim();
+
+    if (!email) {
+      return;
+    }
+     this.bubbleService
+     .getBubblesByEmail(email)
+      .subscribe(bubbles => {this.bubbles = bubbles; console.log(this.bubbles)});
+
+    console.log('Searching for bubbles:', email);
+  }
 
 }
--- cfpbubble/src/app/services/bubble.service.ts
+++ cfpbubble/src/app/services/bubble.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BubbleRequest } from '../dtos/bubble-request';
+import { Team } from '../dtos/team';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,9 @@ export class BubbleService {
 
   createBubble(request: BubbleRequest): Observable<any> {
     return this.http.post<any>(this.apiUrl + "/create", request);
+  }
+
+  getBubblesByEmail(email:string): Observable<any[]>{
+    return this.http.get<any>(this.apiUrl + "?email=" + email);
   }
 }
